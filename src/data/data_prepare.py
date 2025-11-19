97cc0a7ef965e7a68a19438fc9cec7e3de3eafe9
Phoenix377ss <phoenix377ss@gmail.com>
2025-11-19 15:30:22 +0700

    step2

--- src/data/data_prepare.py
+++ src/data/data_prepare.py
@@ -14,6 +14,7 @@ def fillna(dataset: pd.DataFrame) -> pd.DataFrame:
             prepare_dataset[column] = prepare_dataset[column] - 8
             prepare_dataset[column] = prepare_dataset[column] - 71
             prepare_dataset[column] = prepare_dataset[column] - 22
+            prepare_dataset[column] = prepare_dataset[column] - 22
     
     return prepare_dataset
 
